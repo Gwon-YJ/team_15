b724fcf7207225077f357fa781d82321c4ffee80
taeung515 <xodndd515@gmail.com>
2025-06-14 15:28:32 +0900

    feat: 태스크 조회 api 구현
    Resolves: #6

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/task/controller/TaskController.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/task/controller/TaskController.java
@@ -2,15 +2,19 @@ package github.npcamp.teamtaskflow.domain.task.controller;
 
 import github.npcamp.teamtaskflow.domain.task.dto.CreateTaskRequestDto;
 import github.npcamp.teamtaskflow.domain.task.dto.CreateTaskResponseDto;
+import github.npcamp.teamtaskflow.domain.task.dto.TaskResponseDto;
+import github.npcamp.teamtaskflow.domain.task.dto.TaskDetailResponseDto;
 import github.npcamp.teamtaskflow.domain.task.service.TaskService;
 import github.npcamp.teamtaskflow.global.payload.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tasks")
@@ -22,6 +26,18 @@ public class TaskController {
     @PostMapping
     public ResponseEntity<ApiResponse<CreateTaskResponseDto>> createTask(@Valid @RequestBody CreateTaskRequestDto req) {
         CreateTaskResponseDto res = taskService.createTask(req);
-        return ResponseEntity.ok(ApiResponse.success(res));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(res));
+    }
+
+    @GetMapping("/{taskId}")
+    public ResponseEntity<ApiResponse<TaskDetailResponseDto>> getTask(@PathVariable Long taskId) {
+        TaskDetailResponseDto res = taskService.getTask(taskId);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(res));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<TaskResponseDto>>> getTasks(@PageableDefault(sort = "dueDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<TaskResponseDto> tasks = taskService.getTasks(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(tasks));
     }
 }
--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/task/service/TaskServiceImpl.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/task/service/TaskServiceImpl.java
@@ -3,19 +3,31 @@ package github.npcamp.teamtaskflow.domain.task.service;
 import github.npcamp.teamtaskflow.domain.common.entity.Task;
 import github.npcamp.teamtaskflow.domain.task.dto.CreateTaskRequestDto;
 import github.npcamp.teamtaskflow.domain.task.dto.CreateTaskResponseDto;
+import github.npcamp.teamtaskflow.domain.task.dto.TaskResponseDto;
+import github.npcamp.teamtaskflow.domain.task.dto.TaskDetailResponseDto;
+import github.npcamp.teamtaskflow.domain.task.exception.TaskNotFoundException;
 import github.npcamp.teamtaskflow.domain.task.repository.TaskRepository;
+import github.npcamp.teamtaskflow.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
-//    private final UserService userService;
+//    private final UserService userService; TODO
+//    private final CommentService commentService; TODO
 //    private final ActivityLogService activityLogService; TODO
 
+    /**
+     * Task 생성
+     */
     @Override
+    @Transactional
     public CreateTaskResponseDto createTask(CreateTaskRequestDto req) {
 
 //        User assignee = userService.findById(req.getAssigneeId()); TODO
@@ -31,5 +43,31 @@ public class TaskServiceImpl implements TaskService {
         Task savedTask = taskRepository.save(task);
 
         return CreateTaskResponseDto.toDto(savedTask);
+    }
+
+    /**
+     * Task 단건과 댓글 목록을 조회
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public TaskDetailResponseDto getTask(Long taskId) {
+
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new TaskNotFoundException(ErrorCode.TASK_NOT_FOUND));
+
+        // taskId로 커멘트 리스트를 찾아오기 TODO
+
+//        return TaskResponseDto.toDto(task, comments); TODO
+        return TaskDetailResponseDto.toDto(task);
+    }
+
+    /**
+     * Task page조회 defalut size = 10
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Page<TaskResponseDto> getTasks(Pageable pageable) {
+        Page<Task> tasks = taskRepository.findAll(pageable);
+        return tasks.map(TaskResponseDto::toDto);
     }
 }
