d9748f9d20c4047aadb705ed443bfcee049180ef
DaHyun-8509 <luce0359@gmail.com>
2025-06-16 20:48:52 +0900

    resolve #10,21 refactor: 공통 댓글 응답 Dto 생성

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/dto/response/CommentResponseDto.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/dto/response/CommentResponseDto.java
@@ -1,4 +1,4 @@
-package github.npcamp.teamtaskflow.domain.comment.dto;
+package github.npcamp.teamtaskflow.domain.comment.dto.response;
 
 import github.npcamp.teamtaskflow.domain.common.entity.Comment;
 import lombok.Builder;
@@ -6,20 +6,22 @@ import lombok.Builder;
 import java.time.LocalDateTime;
 
 @Builder
-public record CreateCommentResponseDto (
+public record CommentResponseDto(
         Long id,
         Long taskId,
         Long userId,
+        String userName,
         String content,
         LocalDateTime createdAt,
         LocalDateTime updatedAt,
         boolean isDeleted
 ) {
-    public static CreateCommentResponseDto toDto(Comment comment) {
-        return CreateCommentResponseDto.builder()
+    public static CommentResponseDto toDto(Comment comment) {
+        return CommentResponseDto.builder()
                 .id(comment.getId())
                 .taskId(comment.getTask().getId())
                 .userId(comment.getUser().getId())
+                .userName(comment.getUser().getUsername())
                 .content(comment.getContent())
                 .createdAt(comment.getCreatedAt())
                 .updatedAt(comment.getUpdatedAt())
