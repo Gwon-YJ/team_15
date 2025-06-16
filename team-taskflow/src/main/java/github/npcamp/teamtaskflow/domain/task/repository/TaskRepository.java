fd385fef376aa94273a9465d653b9556badea8c8
taeung515 <xodndd515@gmail.com>
2025-06-16 17:36:17 +0900

    feat: 태스크 키워드 검색 기능 추가
    Resolves: #15

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/task/repository/TaskRepository.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/task/repository/TaskRepository.java
@@ -1,8 +1,12 @@
 package github.npcamp.teamtaskflow.domain.task.repository;
 
 import github.npcamp.teamtaskflow.domain.common.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    long countByIsDeletedFalse();
+
+    Page<Task> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String titleKeyword, String contentKeyword, Pageable pageable);
+
 }
