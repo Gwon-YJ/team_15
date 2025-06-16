7e258b87955167371a7fcda26b45a7d012a0d2a2
DaHyun-8509 <luce0359@gmail.com>
2025-06-16 11:40:27 +0900

    refactor: 공통 예외를 적용하도록 extends 변경

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/exception/CommentNotFoundException.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/exception/CommentNotFoundException.java
@@ -1,7 +1,10 @@
 package github.npcamp.teamtaskflow.domain.comment.exception;
 
-public class CommentNotFoundException extends RuntimeException {
-    public CommentNotFoundException(String message) {
-        super(message);
+import github.npcamp.teamtaskflow.global.exception.CustomException;
+import github.npcamp.teamtaskflow.global.exception.ErrorCode;
+
+public class CommentNotFoundException extends CustomException {
+    public CommentNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
     }
 }
