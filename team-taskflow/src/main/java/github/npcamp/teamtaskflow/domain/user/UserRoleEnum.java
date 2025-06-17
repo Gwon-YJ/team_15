03cc9b6f9689bb381d323ac3d4f9e4b7d7e1d245
taeung515 <xodndd515@gmail.com>
2025-06-17 16:01:38 +0900

    feat: userRole 추가
    resolves: #29

---
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/user/UserRoleEnum.java
@@ -0,0 +1,14 @@
+package github.npcamp.teamtaskflow.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UserRoleEnum {
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String role;        // Spring Security 권한 이름
+
+}
