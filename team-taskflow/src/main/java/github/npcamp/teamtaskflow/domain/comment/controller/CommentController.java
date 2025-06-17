01eaae1da657327b606dfb722e52f816964f9fe6
DaHyun-8509 <luce0359@gmail.com>
2025-06-17 18:35:57 +0900

    resolve #10, #21, #24 refactor: 토큰 받도록 컨트롤러 수정

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/controller/CommentController.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/controller/CommentController.java
@@ -14,6 +14,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
@@ -24,40 +26,50 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    //댓글생성
     @PostMapping
     public ResponseEntity<ApiResponse<CommentResponseDto>> createComment(
             @PathVariable Long taskId,
-            @Valid @RequestBody CreateCommentRequestDto requestDto
+            @Valid @RequestBody CreateCommentRequestDto requestDto,
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
-        CommentResponseDto responseDto = commentService.createComment(taskId, requestDto);
+        String username = userDetails.getUsername();
+        CommentResponseDto responseDto = commentService.createComment(taskId, username, requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(responseDto));
     }
 
+    //댓글 전체 조회
     @GetMapping
     public ResponseEntity<ApiResponse<Page<CommentResponseListDto>>> getComments(
             @PathVariable Long taskId,
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ) {
         Page<CommentResponseListDto> responseDto = commentService.getComments(taskId, pageable);
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(responseDto));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(responseDto));
     }
 
+    //댓글 수정 - content만 수정하므로, patch사용함.
     @PatchMapping("/{commentId}")
     public ResponseEntity<ApiResponse<CommentResponseDto>> updateComment(
             @PathVariable Long taskId,
             @PathVariable Long commentId,
-            @Valid @RequestBody UpdateCommentRequestDto requestDto
+            @Valid @RequestBody UpdateCommentRequestDto requestDto,
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
-        CommentResponseDto responseDto = commentService.updateContent(taskId, commentId, requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(responseDto));
+        String username = userDetails.getUsername();
+        CommentResponseDto responseDto = commentService.updateContent(taskId, commentId, username, requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(responseDto));
     }
 
+    //댓글삭제 (soft delete)
     @DeleteMapping("/{commentId}")
     public ResponseEntity<ApiResponse<CommentDeleteResponseDto>> deleteComment(
             @PathVariable Long taskId,
-            @PathVariable Long commentId
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
-        CommentDeleteResponseDto responseDto = commentService.deleteComment(taskId, commentId);
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("댓글이 성공적으로 삭제되었습니다."));
+        String username = userDetails.getUsername();
+        CommentDeleteResponseDto responseDto = commentService.deleteComment(taskId, commentId, username);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("댓글이 성공적으로 삭제되었습니다."));
     }
 }
