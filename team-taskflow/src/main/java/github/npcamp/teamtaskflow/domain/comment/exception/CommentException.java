ee48ac0dd1685f1234c793875a1b67c1b20c0690
DaHyun-8509 <luce0359@gmail.com>
2025-06-17 11:08:40 +0900

    refactor: 가독성에 맞게 파일명 리팩토링

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/exception/CommentException.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/exception/CommentException.java
@@ -3,8 +3,8 @@ package github.npcamp.teamtaskflow.domain.comment.exception;
 import github.npcamp.teamtaskflow.global.exception.CustomException;
 import github.npcamp.teamtaskflow.global.exception.ErrorCode;
 
-public class CommentNotFoundException extends CustomException {
-    public CommentNotFoundException(ErrorCode errorCode) {
+public class CommentException extends CustomException {
+    public CommentException(ErrorCode errorCode) {
         super(errorCode);
     }
 }
