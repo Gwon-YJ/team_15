40dab2d425ba80f62ef3767000ec269e4a0d4ae3
DaHyun-8509 <luce0359@gmail.com>
2025-06-17 11:32:19 +0900

    refactor: comment 에러 추가

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/global/exception/ErrorCode.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/global/exception/ErrorCode.java
@@ -13,10 +13,11 @@ public enum ErrorCode {
 
     // task
     TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시물을 찾을 수 없습니다."),
-    INVALID_STATUS_TRANSITION(HttpStatus.BAD_REQUEST, "TODO → IN_PROGRESS → DONE 순서로만 변경할 수 있습니다.");
+    INVALID_STATUS_TRANSITION(HttpStatus.BAD_REQUEST, "TODO → IN_PROGRESS → DONE 순서로만 변경할 수 있습니다."),
 
     // comment
-
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글을 찾을 수 없습니다."),
+    TASK_COMMENT_MISMATCH(HttpStatus.BAD_REQUEST,"댓글이 해당 할일(task)에 속하지 않습니다");
     // board
 
     // log
