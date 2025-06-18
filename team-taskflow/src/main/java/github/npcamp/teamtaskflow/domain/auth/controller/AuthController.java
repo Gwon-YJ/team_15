701258755bc9a70795471b9894a6691f0661bf60
youngee2 <jr3537@naver.com>
2025-06-18 16:55:15 +0900

    feat: 회원 탈퇴 api 기능 구현 Resolves: #43

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/auth/controller/AuthController.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/auth/controller/AuthController.java
@@ -2,6 +2,7 @@ package github.npcamp.teamtaskflow.domain.auth.controller;
 
 import github.npcamp.teamtaskflow.domain.auth.dto.request.LoginRequestDto;
 import github.npcamp.teamtaskflow.domain.auth.dto.request.RegisterRequestDto;
+import github.npcamp.teamtaskflow.domain.auth.dto.request.WithdrawRequestDto;
 import github.npcamp.teamtaskflow.domain.auth.dto.response.AuthResponseDto;
 import github.npcamp.teamtaskflow.domain.auth.service.AuthService;
 import github.npcamp.teamtaskflow.global.payload.ApiResponse;
@@ -9,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +36,13 @@ public class AuthController {
         AuthResponseDto res = authService.login(req);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("로그인이 완료되었습니다", res));
     }
+
+    //회원 탈퇴
+    @PostMapping("/withdraw")
+    public ResponseEntity<ApiResponse<Void>> withDraw(@Valid @RequestBody WithdrawRequestDto req, @AuthenticationPrincipal Long userid) {
+        authService.withdraw(req,userid);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("회원 탈퇴가 완료되었습니다."));
+    }
+
 
 }
