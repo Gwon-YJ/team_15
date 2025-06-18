ed1a1517300bb1431bd41796725b43ac0a364148
DaHyun-8509 <luce0359@gmail.com>
2025-06-18 12:36:38 +0900

    resolve #40 refactor: postman 응답 오류 수정

--- team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/service/CommentServiceImpl.java
+++ team-taskflow/src/main/java/github/npcamp/teamtaskflow/domain/comment/service/CommentServiceImpl.java
@@ -34,18 +34,19 @@ public class CommentServiceImpl implements CommentService{
     @Transactional
     public CommentResponseDto createComment(Long taskId, Long userId, CreateCommentRequestDto requestDto) {
 
-        Task task = taskService.findTaskByIdOrElseThrow(taskId);
-        User user = userService.findUserByIdOrElseThrow(userId);
+        Task task = taskService.findTaskByIdOrElseThrow(taskId); // 태스크 존재 여부 확인
+        User user = userService.findUserByIdOrElseThrow(userId); // 사용자 존재 여부 확인
 
-        Comment comment = Comment.builder()
+        Comment comment = Comment.builder() // 댓글 객체 생성
                 .task(task)
                 .user(user)
+                .username(user.getUsername())
                 .content(requestDto.content())
                 .build();
 
-        Comment saved = commentRepository.save(comment);
+        Comment saved = commentRepository.save(comment); // 댓글 저장
 
-        return CommentResponseDto.toDto(saved);
+        return CommentResponseDto.toDto(saved); // 응답 DTO 반환
     }
 
 
@@ -53,9 +54,10 @@ public class CommentServiceImpl implements CommentService{
     @Override
     @Transactional(readOnly = true)
     public Page<CommentResponseListDto> getComments(Long taskId, Pageable pageable) {
-        Page<Comment> comments = commentRepository.findAllByTaskId(taskId, pageable);
 
-        return comments.map(CommentResponseListDto::toDto);
+        Page<Comment> comments = commentRepository.findAllByTaskId(taskId, pageable); // 태스크 ID로 댓글 목록 조회
+
+        return comments.map(CommentResponseListDto::toDto);// 응답 DTO로 변환
     }
 
     //댓글 수정
@@ -66,28 +68,27 @@ public class CommentServiceImpl implements CommentService{
         Task task = taskService.findTaskByIdOrElseThrow(taskId);
         Comment comment = findCommentByIdOrElseThrow(commentId);
 
+        // 댓글이 해당 task에 속하지 않는 경우 예외처리
         if (!comment.getTask().getId().equals(task.getId())) {
             throw new CommentException(ErrorCode.TASK_COMMENT_MISMATCH);
         }
 
-        User user = userService.findUserByIdOrElseThrow(userId);
-        if (!comment.getUser().getId().equals(user.getId())) {
+        User user = userService.findUserByIdOrElseThrow(userId); // 사용자 존재 확인
+        if (!comment.getUser().getId().equals(user.getId())) { // 댓글 본인 확인 - 아니면 에러코드
             throw new CommentException(ErrorCode.UNAUTHORIZED_COMMENT_ACCESS);
         }
 
-        comment.updateComment(requestDto.content());
+        comment.updateComment(requestDto.content()); // 댓글 내용 수정
 
-        return CommentResponseDto.toDto(comment);
+        return CommentResponseDto.toDto(comment); // 수정된 댓글 반환
     }
 
+    //댓글 삭제
     @Override
     @Transactional
     public CommentDeleteResponseDto deleteComment(Long taskId, Long commentId, Long userId) {
 
-        // Task 존재 확인
         Task task = taskService.findTaskByIdOrElseThrow(taskId);
-
-        // Comment 존재 확인
         Comment comment = findCommentByIdOrElseThrow(commentId);
 
         if (!comment.getTask().getId().equals(task.getId())) {
@@ -99,12 +100,12 @@ public class CommentServiceImpl implements CommentService{
             throw new CommentException(ErrorCode.UNAUTHORIZED_COMMENT_ACCESS);
         }
 
-        commentRepository.delete(comment);
+        commentRepository.delete(comment); // 댓글 내용 삭제 (soft)
 
-        return CommentDeleteResponseDto.toDto(comment);
+        return CommentDeleteResponseDto.toDto(comment); //삭제된 댓글 반환
     }
 
-    //commentId 찾는 메서드
+    // commentId로 댓글을 조회하고 없으면 예외
     @Override
     @Transactional(readOnly = true)
     public Comment findCommentByIdOrElseThrow(Long commentId) {
