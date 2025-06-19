b7c92678194a6f500baec2fcfcb0d8f665f60374
권용준 <yoyo91828h@gmail.com>
2025-06-19 20:47:00 +0900

    feat: 테스트 코드 작성 #63

--- team-taskflow/src/test/java/github/npcamp/teamtaskflow/domain/user/service/UserServiceTest.java
+++ team-taskflow/src/test/java/github/npcamp/teamtaskflow/domain/user/service/UserServiceTest.java
@@ -1,15 +1,16 @@
 package github.npcamp.teamtaskflow.domain.user.service;
 
-
-
+import github.npcamp.teamtaskflow.domain.common.entity.User;
 import github.npcamp.teamtaskflow.domain.user.UserRoleEnum;
+import github.npcamp.teamtaskflow.domain.user.dto.response.UserResponseDto;
 import github.npcamp.teamtaskflow.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.userdetails.User;
+
+
 
 import java.util.List;
 import java.util.Optional;
@@ -26,41 +27,41 @@ class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
-//    @Test
-//    void 전체_조회_성공() {
-//        // given
-//        List<User> userList = List.of(
-//                new User(1L,"user1", "user1@example.com", "password", "이순신", UserRoleEnum.USER),
-//                new User(1L,"user2", "user2@example.com", "password", "홍길동", UserRoleEnum.USER)
-//        );
-//
-//        when(userRepository.findAll()).thenReturn(userList);
-//
-//        // when
-//        List<User> result = userService.getAllUsers();
-//
-//        // then
-//        assertThat(result).hasSize(2);
-//        verify(userRepository, times(1)).findAll();
-//    }
-//
-//    @Test
-//    void 단일_조회_성공() {
-//        // given
-//        User user = new User(1L,"user1", "user1@example.com", "password", "이순신", UserRoleEnum.USER);
-//        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-//
-//        // when
-//        User result = userService.getUserById(1L);
-//
-//        // then
-//        assertThat(result.getUsername()).isEqualTo("user1");
-//    }
-//
+    @Test
+    void 전체_조회_성공() {
+        // given
+        List<User> userList = List.of(
+                new User(1L,"user1", "user1@example.com", "password", "이순신", UserRoleEnum.USER,false,null),
+                new User(2L,"user2", "user2@example.com", "password", "홍길동", UserRoleEnum.USER,false, null)
+        );
+
+        when(userRepository.findAll()).thenReturn(userList);
+
+        // when
+        List<UserResponseDto> result = userService.getUsers();
+
+        // then
+        assertThat(result).hasSize(2);
+        verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    void 단일_조회_성공() {
+        // given
+        User user = new User(1L,"user1", "user1@example.com", "password", "이순신", UserRoleEnum.USER,false,null);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        // when
+        User result = userService.findUserByIdOrElseThrow(1L);
+
+        // then
+        assertThat(result.getUsername()).isEqualTo("user1");
+    }
+
 //    @Test
 //    void deleteUser_삭제_성공() {
 //        // given
-//        User user = new User( 1L,"user1", "user1@example.com", "password", "이순신", UserRoleEnum.USER);
+//        User user = new User( 1L,"user1", "user1@example.com", "password", "이순신", UserRoleEnum.USER,false,null);
 //        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 //
 //        // when
@@ -70,3 +71,4 @@ class UserServiceTest {
 //        verify(userRepository, times(1)).delete(user);
 //    }
 }
+
